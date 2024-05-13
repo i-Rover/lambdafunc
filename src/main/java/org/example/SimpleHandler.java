128a20ac4543d46d72475d2ac95c4bc3d22b3aa4
syamilsyamil <muhammadsyamil@milradius.com.my>
2024-05-13 15:42:19 +0800

    Remove Request Model + Unnessary Dependencies
    Reduce the file size to upload in AWS Lambda

--- src/main/java/org/example/SimpleHandler.java
+++ src/main/java/org/example/SimpleHandler.java
@@ -1,27 +1,39 @@
 package org.example;
 
-import RequestModel.PushMethodMainRequestModel;
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
+import java.util.Map;
 
-public class SimpleHandler implements RequestHandler<PushMethodMainRequestModel, String> {
-    private final ObjectMapper objectMaper;
-
-    public SimpleHandler(ObjectMapper objectMaper) {
-        this.objectMaper = objectMaper;
-    }
-
-    public String handleRequest(PushMethodMainRequestModel input, Context context){
-        LambdaLogger logger = context.getLogger();
-        logger.log("JDK Version: "+ System.getProperty("java.version"));
-        try {
-            return objectMaper.writeValueAsString(input);
-        } catch (JsonProcessingException e) {
-            System.out.println(e.toString());
-            throw new RuntimeException(e);
+public class SimpleHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+    @Override
+    public Map<String, Object> handleRequest(Map<String, Object> event, Context context) {
+        // Log the incoming event
+        System.out.println("Received event: " + event);
+        // Initialize headers
+        Map<String, String> headers = new HashMap<>();
+        // Copy Location header from incoming headers
+        if (event.containsKey("header")) {
+            Map<String, String> incomingHeaders = (Map<String, String>) event.get("header");
+            if (incomingHeaders.containsKey("Location")) {
+                headers.put("Location", incomingHeaders.get("Location"));
+            }
         }
+        // Copy query parameter "code" to bodyResponse if present
+        Map<String, Object> bodyResponse = new HashMap<>(event);
+        if (event.containsKey("queryStringParameters")) {
+            Map<String, String> queryParams = (Map<String, String>) event.get("queryStringParameters");
+            if (queryParams.containsKey("code")) {
+                bodyResponse.put("code", queryParams.get("code"));
+            }
+        }
+        // Construct response
+        Map<String, Object> response = new HashMap<>();
+        response.put("statusCode", 200);
+        response.put("headers", headers);
+        response.put("body", bodyResponse);
+        // Log the response
+        System.out.println("Sending response: " + response);
+        return response;
     }
 }
